8231c8d148cbe5a1df38d8d1650b8eb755ec9d32
David Velinga <davidvelinga@outlook.com>
2018-01-23 14:51:07 +0100

    Initial commit

---
+++ dura-vermeer-dashboard/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dura_vermeer_dashboard.Controllers
+{
+  [Route("api/[controller]")]
+  public class ValuesController: Controller
+  {
+    [HttpGet]
+    public IEnumerable<string> Get() {
+      return new string[] { "Hello", "World" };
+    }
+  }
+}
