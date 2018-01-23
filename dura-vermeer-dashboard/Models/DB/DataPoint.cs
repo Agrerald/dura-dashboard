c989709543fcf10c2479791f938a9a6018ef55e4
David Velinga <davidvelinga@outlook.com>
2018-01-23 15:47:36 +0100

    EntityFramework Core toegevoegd

---
+++ dura-vermeer-dashboard/Models/DB/DataPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dura_vermeer_dashboard.Models.DB
+{
+    public partial class DataPoint
+    {
+        public int DataPointId { get; set; }
+        public string Naam { get; set; }
+        public DateTime? Datum { get; set; }
+        public int Rssi { get; set; }
+        public int Afstand { get; set; }
+        public int NodeId { get; set; }
+
+        public Node Node { get; set; }
+    }
+}
---
+++ dura-vermeer-dashboard/Models/DB/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dura_vermeer_dashboard.Models.DB
+{
+    public partial class Node
+    {
+        public Node()
+        {
+            DataPoint = new HashSet<DataPoint>();
+        }
+
+        public int NodeId { get; set; }
+        public string Naam { get; set; }
+        public string Locatie { get; set; }
+        public string Latitude { get; set; }
+        public string Longtitude { get; set; }
+
+        public ICollection<DataPoint> DataPoint { get; set; }
+    }
+}
