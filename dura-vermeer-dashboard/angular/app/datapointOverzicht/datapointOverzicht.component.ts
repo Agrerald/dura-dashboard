05a07b8d2fb6bd835251e72328b3346c90f4a0a4
lb321 <lucas.001@live.nl>
2018-01-31 17:04:40 +0100

    Rondetijden filtering en kalmanfilter op afstand-tijd grafiek

--- dura-vermeer-dashboard/angular/app/datapointOverzicht/datapointOverzicht.component.ts
+++ dura-vermeer-dashboard/angular/app/datapointOverzicht/datapointOverzicht.component.ts
@@ -3,6 +3,8 @@ import { Component } from "@angular/core";
 import * as Highcharts from "Highcharts";
 import {SelectedGatewayService} from "../gatewaySelector/selectedGateway.service";
 import {HttpService} from "../http.service";
+import KalmanFilter from 'kalmanjs';
+
 
 @Component({
   selector: 'DatapointOverzichtComponent',
@@ -18,6 +20,43 @@ export class DatapointOverzichtComponent {
     },
     title: {
       text: 'Geregistreerde datapunten'
+    },
+    credits: {
+      enabled: false
+    },
+    yAxis: {
+      title: {
+        text: 'Afstand (centimeter)'
+      }
+    },
+    xAxis: {
+      type: 'datetime',
+      title: {
+        text: 'Tijd'
+      }
+    },
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br>',
+      pointFormat: '{point.x:%e. %b %H:%M:%S}: {point.y:.2f} cm'
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          enabled: true
+        }
+      }
+    },
+    series: []
+  });
+  filteredChart = new Chart({
+    chart: {
+      type: 'scatter'
+    },
+    title: {
+      text: 'Geregistreerde datapunten'
+    },
+    subtitle: {
+      text: 'Gefilterd met Kalmanfilter'
     },
     credits: {
       enabled: false
@@ -62,15 +101,37 @@ export class DatapointOverzichtComponent {
     while(this.chart.options.series.length > 0) {
       this.chart.removeSerie(0);
     }
+    while(this.filteredChart.options.series.length > 0) {
+      this.filteredChart.removeSerie(0);
+    }
     this.httpService.getDataPointsOnDay(new Date(Date.parse(this.vanDatum) - 1000 * 60 * 60), new Date(Date.parse(this.totDatum) - 1000 * 60 * 60), this.selectedNodeId ).subscribe(datapoints => {
       const dp = {};
       for (const datapoint of datapoints){
         if(!dp[datapoint.Naam]) dp[datapoint.Naam] = [];
         dp[datapoint.Naam].push([datapoint.Datum.getTime(), datapoint.Afstand]);
       }
+
       for (const key in dp){
-        console.log(dp[key]);
         this.chart.addSerie({
+          name: key,
+          data: dp[key]
+        });
+      }
+      for (const key in dp) {
+        let distances = [];
+        for (const datapoint of dp[key]){
+          distances.push(datapoint[1]);
+        }
+        const kalmanFilter = new KalmanFilter({R: 0.01, Q: 3});
+
+        const dataConstantKalman = distances.map(function(v) {
+          return kalmanFilter.filter(v);
+        });
+
+        for (const i in dataConstantKalman){
+          dp[key][i][1] = dataConstantKalman[i];
+        }
+        this.filteredChart.addSerie({
           name: key,
           data: dp[key]
         });
--- dura-vermeer-dashboard/angular/app/rondetijden/rondetijd.component.ts
+++ dura-vermeer-dashboard/angular/app/rondetijden/rondetijd.component.ts
@@ -8,6 +8,7 @@ import {HttpService} from "../http.service";
 })
 export class RondetijdComponent {
   public rondeTijden = {};
+  public minRondetijd = 10;
   public vanDatum: string = new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString().substr(0, 16);
   public totDatum: string = new Date(Date.now()).toISOString().substr(0, 16);
 
@@ -24,42 +25,36 @@ export class RondetijdComponent {
     yAxis: {
       title: {
         text: 'Duur ronde (seconden)'
+      },
+      stackLabels: {
+        enabled: true,
+        style: {
+          fontWeight: 'bold'
+        }
       }
     },
+    xAxis: {
+      title: {
+        text: 'Rondenummer'
+      }
+    },
+    tooltip: {
+      headerFormat: '<b>Ronde {point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y} sec.<br/>Totaal: {point.stackTotal} sec.'
+    },
     plotOptions: {
-      spline: {
-        marker: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
           enabled: true
         }
-      },
-      column: {
-        stacking: 'normal'
       }
     },
     series: []
   });
 
   constructor(public httpService: HttpService) {
-    this.chart.addSerie({
-      name: 'BlueUp-01-016167 naar 2de',
-      data: [56, 52, 60],
-      stack: "67"
-    });
-    this.chart.addSerie({
-      name: 'BlueUp-01-016167 naar 4de',
-      data: [53, 56, 56],
-      stack: "67"
-    });
-    this.chart.addSerie({
-      name: 'BlueUp-01-016166 naar 2de',
-      data: [50, 78, 109],
-      stack: "66"
-    });
-    this.chart.addSerie({
-      name: 'BlueUp-01-016166 naar 4de',
-      data: [54, 51, 88],
-      stack: "66"
-    });
+
   }
 
   public drawChart() {
@@ -69,11 +64,10 @@ export class RondetijdComponent {
     this.httpService.getRondetijden(new Date(Date.parse(this.vanDatum) - 1000 * 60 * 60), new Date(Date.parse(this.totDatum) - 1000 * 60 * 60)).subscribe(rondetijden => {
       for (const key in rondetijden) {
         const series = {};
-        console.log(key);
         for (const ronde of rondetijden[key]) {
           const seriename = key + ': ' + ronde['fromNode'] + '->' + ronde['toNode'];
           if (!series[seriename]) series[seriename] = {name: seriename,  data: [], stack: key};
-          series[seriename]['data'].push(ronde['rondeDuur']);
+          if (ronde['rondeDuur'] > this.minRondetijd) series[seriename]['data'].push(ronde['rondeDuur']);
         }
         for (const serie in series){
           this.chart.addSerie(series[serie]);
@@ -81,4 +75,5 @@ export class RondetijdComponent {
       }
     });
   }
+
 }
